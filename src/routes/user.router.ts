d4471f12e1cd67a8114d3684a450a1cd8f1ade46
Jeison Lasprilla <101517638+JeisonLasprilla@users.noreply.github.com>
2024-09-12 16:39:19 -0500

    export user.routers sin default

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -5,13 +5,13 @@ import authMiddleware from '../middlewares/auth';
 import validateSchema from '../middlewares/validateSchema';
 import userSchema from '../schemas/user.schema';
 
-const router = express.Router();
+export const router = express.Router();
 
 // Rutas públicas
 router.post('/login', UserController.login);
 
 // Rutas protegidas
-router.use(authMiddleware);
+//router.use(authMiddleware);
 
 // Ruta para obtener todos los usuarios (solo para usuarios autenticados)
 router.get('/', UserController.getAll);
@@ -20,11 +20,10 @@ router.get('/', UserController.getAll);
 router.get('/profile', UserController.getUser);
 
 // Rutas que requieren ser superadmin
-router.post('/', authMiddleware.isSuperAdmin, validateSchema(userSchema), UserController.create);
+//router.post('/', authMiddleware.isSuperAdmin, validateSchema(userSchema), UserController.create);
+router.post('/', validateSchema(userSchema), UserController.create);
 router.put('/:id', authMiddleware.isSuperAdmin, validateSchema(userSchema), UserController.update);
 router.delete('/:id', authMiddleware.isSuperAdmin, UserController.delete);
 
 // Ruta para obtener un usuario específico (accesible para todos los usuarios autenticados)
 router.get('/:id', UserController.getUser);
-
-export default router;
